e2ef7b706775def7c97ae848a2c019e96f93042b
uxsu <uxsu.programming@gmail.com>
2025-01-07 08:30:30 +0100

    feat(args): Add convenient placeholder/binder.
    For simple use cases, instead of writing lambdas
    one can use the the arg class to create
    fubction on the fly.
    
    Examples:
    using namespace injectx::args::numbered;
    
    std::sort(v.begin(), v.end(), _1 < _2);
    
    ...
    
    using namespace injectx::args;
    constexpr auto IsTab  = arg == '\t';
    constexpr auto IsSpace = arg == ' ';
    constexpr auto IsCarriageReturn = arg == '\r';
    constexpr auto IsNewLine = arg == '\n';
    
    constexpr auto IsWhitespace = IsTab || IsSpace
      || IsCarriageReturn || IsNewLine;
    
    std::find_if(s.begin(), s.end(), IsWhitespace);

---
+++ src/args/include/injectx/args/args.hpp
@@ -0,0 +1,247 @@
+#pragma once
+
+#include <functional>
+#include <tuple>
+#include <type_traits>
+#include <utility>
+
+namespace injectx::args {
+namespace details {
+
+template<typename Tuple, typename Callable, int arg_position>
+constexpr auto call(Tuple t, Callable callable) {
+  return callable(std::get<arg_position>(t));
+}
+
+// Required to have a way to differentiate between arg and other types
+struct ArgTag {};
+
+template<typename T>
+concept ArgLike = requires {
+  typename std::remove_cvref_t<T>::Tag;
+  typename std::remove_cvref_t<T>::ContextTag;
+  requires std::same_as<typename std::remove_cvref_t<T>::Tag, ArgTag>;
+};
+
+template<typename ArgLike1, typename ArgLike2>
+concept SharesContextWith = requires {
+  requires ArgLike<ArgLike1>;
+  requires ArgLike<ArgLike2>;
+  requires std::derived_from<
+               typename std::remove_cvref_t<ArgLike1>::ContextTag,
+               typename std::remove_cvref_t<ArgLike2>::ContextTag>
+               || std::derived_from<
+                   typename std::remove_cvref_t<ArgLike2>::ContextTag,
+                   typename std::remove_cvref_t<ArgLike1>::ContextTag>;
+};
+
+template<ArgLike ArgLike1, ArgLike ArgLike2>
+struct common_context : std::conditional<
+                            std::derived_from<
+                                typename ArgLike1::ContextTag,
+                                typename ArgLike2::ContextTag>,
+                            typename ArgLike1::ContextTag,
+                            typename ArgLike2::ContextTag> {};
+
+template<ArgLike ArgLike1, ArgLike ArgLike2>
+using common_context_t = typename common_context<ArgLike1, ArgLike2>::type;
+
+template<class Callable, int arg_position, typename Context>
+struct Arg {
+  using Tag = ArgTag;
+  using ContextTag = Context;
+  Callable callable_;
+
+  constexpr Arg(Callable&& callable)
+      : callable_(callable) {
+  }
+
+  template<typename... Args>
+  constexpr auto operator()(Args&&... args) const {
+    if constexpr (arg_position != -1) {
+      auto t = std::make_tuple(args...);
+      return details::call<decltype(t), Callable, arg_position>(t, callable_);
+    } else {
+      return callable_(args...);
+    }
+  }
+
+  template<typename... Args>
+  constexpr auto operator()(Args&... args) const {
+    if constexpr (arg_position != -1) {
+      auto t = std::make_tuple(args...);
+      return details::call<decltype(t), Callable, arg_position>(t, callable_);
+    } else {
+      return callable_(args...);
+    }
+  }
+
+  template<typename Operator, ArgLike OtherArgLike>
+  constexpr auto thisComposeOther(OtherArgLike f, Operator) const {
+    static_assert(
+        SharesContextWith<OtherArgLike, Arg<Callable, arg_position, Context>>,
+        "The arguments must share the same context, make sure not to mix "
+        "arguments from unrelated namespaces.");
+    auto composed = [l = *this, r = f](auto&&... args) {
+      Operator op{};
+      return op(l(args...), r(args...));
+    };
+    using CommonContext =
+        common_context_t<OtherArgLike, Arg<Callable, arg_position, Context>>;
+    return Arg<decltype(composed), -1, CommonContext>(std::move(composed));
+  }
+
+  template<typename Value, typename Operator>
+  constexpr auto valueComposeThis(Value v, Operator) const {
+    auto composed = [callable = *this, v](auto&&... args) {
+      Operator op{};
+      return op(v, callable(args...));
+    };
+    return Arg<decltype(composed), -1, Context>(std::move(composed));
+  }
+
+  template<typename Value, typename Operator>
+  constexpr auto thisComposeValue(Value v, Operator) const {
+    auto composed = [callable = *this, v](auto&&... args) {
+      Operator op{};
+      return op(callable(args...), v);
+    };
+    return Arg<decltype(composed), -1, Context>(std::move(composed));
+  }
+
+  template<typename Operator>
+  constexpr auto thisComposeUnary(Operator) const {
+    auto composed = [callable = *this](auto&&... args) {
+      Operator op{};
+      return op(callable(args...));
+    };
+    return Arg<decltype(composed), -1, Context>(std::move(composed));
+  }
+};
+
+#define ARGLIKE_UNARY_OPERATOR_OVERLOADING(OPERATOR, FUNCTIONAL_WRAPPER) \
+  constexpr auto operator OPERATOR(ArgLike auto&& arglike) {             \
+    return arglike.thisComposeUnary(FUNCTIONAL_WRAPPER{});               \
+  }
+
+#define ARGLIKE_BINARY_OPERATOR_OVERLOADING(OPERATOR, FUNCTIONAL_WRAPPER) \
+  constexpr auto operator OPERATOR(                                       \
+      ArgLike auto&& arglike1, ArgLike auto&& arglike2) {                 \
+    return arglike1.thisComposeOther(arglike2, FUNCTIONAL_WRAPPER{});     \
+  }                                                                       \
+                                                                          \
+  template<typename T>                                                    \
+    requires(!ArgLike<T>)                                                 \
+  constexpr auto operator OPERATOR(ArgLike auto&& arglike, T&& value) {   \
+    return arglike.thisComposeValue(value, FUNCTIONAL_WRAPPER{});         \
+  }                                                                       \
+                                                                          \
+  template<typename T>                                                    \
+    requires(!ArgLike<T>)                                                 \
+  constexpr auto operator OPERATOR(T&& value, ArgLike auto&& arglike) {   \
+    return arglike.valueComposeThis(value, FUNCTIONAL_WRAPPER{});         \
+  }
+
+ARGLIKE_BINARY_OPERATOR_OVERLOADING(+, std::plus)
+ARGLIKE_BINARY_OPERATOR_OVERLOADING(-, std::minus)
+ARGLIKE_BINARY_OPERATOR_OVERLOADING(*, std::multiplies)
+ARGLIKE_BINARY_OPERATOR_OVERLOADING(/, std::divides)
+ARGLIKE_BINARY_OPERATOR_OVERLOADING(%, std::modulus)
+ARGLIKE_UNARY_OPERATOR_OVERLOADING(-, std::negate)
+
+ARGLIKE_BINARY_OPERATOR_OVERLOADING(==, std::equal_to)
+ARGLIKE_BINARY_OPERATOR_OVERLOADING(!=, std::not_equal_to)
+ARGLIKE_BINARY_OPERATOR_OVERLOADING(>, std::greater)
+ARGLIKE_BINARY_OPERATOR_OVERLOADING(>=, std::greater_equal)
+ARGLIKE_BINARY_OPERATOR_OVERLOADING(<, std::less)
+ARGLIKE_BINARY_OPERATOR_OVERLOADING(<=, std::less_equal)
+ARGLIKE_BINARY_OPERATOR_OVERLOADING(<=>, std::compare_three_way)
+
+ARGLIKE_BINARY_OPERATOR_OVERLOADING(&&, std::logical_and)
+ARGLIKE_BINARY_OPERATOR_OVERLOADING(||, std::logical_or)
+ARGLIKE_UNARY_OPERATOR_OVERLOADING(!, std::logical_not)
+
+ARGLIKE_BINARY_OPERATOR_OVERLOADING(&, std::bit_and)
+ARGLIKE_BINARY_OPERATOR_OVERLOADING(|, std::bit_or)
+ARGLIKE_BINARY_OPERATOR_OVERLOADING(^, std::bit_xor)
+ARGLIKE_UNARY_OPERATOR_OVERLOADING(~, std::bit_not)
+
+template<int Number, typename Context>
+constexpr auto makeArg() {
+  return Arg<std::identity, Number, Context>(std::identity{});
+}
+
+namespace contexts {
+struct NoContextTag {};
+
+struct NumberedContextTag {};
+
+struct xyContextTag {};
+
+struct xyzContextTag : xyContextTag {};
+
+struct abContextTag {};
+
+struct abcContextTag : abContextTag {};
+
+struct ijContextTag {};
+
+struct ijkContextTag : ijContextTag {};
+
+struct uvContextTag {};
+
+}  // namespace contexts
+}  // namespace details
+
+constexpr auto arg = details::makeArg<0, details::contexts::NoContextTag>();
+
+namespace numbered {
+constexpr auto _1 =
+    details::makeArg<0, details::contexts::NumberedContextTag>();
+constexpr auto _2 =
+    details::makeArg<1, details::contexts::NumberedContextTag>();
+constexpr auto _3 =
+    details::makeArg<2, details::contexts::NumberedContextTag>();
+constexpr auto _4 =
+    details::makeArg<3, details::contexts::NumberedContextTag>();
+constexpr auto _5 =
+    details::makeArg<4, details::contexts::NumberedContextTag>();
+}  // namespace numbered
+
+namespace xy {
+constexpr auto x = details::makeArg<0, details::contexts::xyContextTag>();
+constexpr auto y = details::makeArg<1, details::contexts::xyContextTag>();
+
+}  // namespace xy
+
+namespace xyz {
+using namespace xy;
+constexpr auto z = details::makeArg<2, details::contexts::xyzContextTag>();
+}  // namespace xyz
+
+namespace ab {
+constexpr auto a = details::makeArg<0, details::contexts::abContextTag>();
+constexpr auto b = details::makeArg<1, details::contexts::abContextTag>();
+}  // namespace ab
+
+namespace abc {
+using namespace ab;
+constexpr auto c = details::makeArg<2, details::contexts::abcContextTag>();
+}  // namespace abc
+
+namespace ij {
+constexpr auto i = details::makeArg<0, details::contexts::ijContextTag>();
+constexpr auto j = details::makeArg<1, details::contexts::ijContextTag>();
+}  // namespace ij
+
+namespace ijk {
+using namespace ij;
+constexpr auto k = details::makeArg<2, details::contexts::ijkContextTag>();
+}  // namespace ijk
+
+namespace uv {
+constexpr auto u = details::makeArg<0, details::contexts::uvContextTag>();
+constexpr auto v = details::makeArg<1, details::contexts::uvContextTag>();
+}  // namespace uv
+
+}  // namespace injectx::args
---
+++ tests/args/args_test.cpp
@@ -0,0 +1,182 @@
+#include "injectx/args/args.hpp"
+
+#include <catch2/catch_test_macros.hpp>
+
+namespace injectx::args::tests {
+
+TEST_CASE("args::arg") {
+  using namespace args;
+  constexpr auto add = arg + 5;
+  REQUIRE(add(10) == 15);
+
+  constexpr auto subtract = arg - 'a';
+  REQUIRE(subtract('b') == 1);
+
+  constexpr auto multiply = arg * arg;
+  REQUIRE(multiply(5.0) == 25.0);
+
+  constexpr auto divide = arg / 5;
+  REQUIRE(divide(25) == 5);
+  REQUIRE(divide(1.0) == 0.2);
+
+  constexpr auto modulo = arg % 5;
+  REQUIRE(modulo(4) == 4);
+  REQUIRE(modulo(7) == 2);
+
+  constexpr auto negate = -arg;
+  REQUIRE(negate(5) == -5);
+
+  constexpr auto equal = arg == 5;
+  REQUIRE_FALSE(equal(4));
+  REQUIRE(equal(5));
+  REQUIRE_FALSE(equal(6));
+
+  constexpr auto notEqual = arg != 5;
+  REQUIRE(notEqual(4));
+  REQUIRE_FALSE(notEqual(5));
+  REQUIRE(notEqual(6));
+
+  constexpr auto greater = arg > 5;
+  REQUIRE_FALSE(greater(4));
+  REQUIRE_FALSE(greater(5));
+  REQUIRE(greater(6));
+
+  constexpr auto greaterEqual = arg >= 5;
+  REQUIRE_FALSE(greaterEqual(4));
+  REQUIRE(greaterEqual(5));
+  REQUIRE(greaterEqual(6));
+
+  constexpr auto less = arg < 5;
+  REQUIRE(less(4));
+  REQUIRE_FALSE(less(5));
+  REQUIRE_FALSE(less(6));
+
+  constexpr auto lessEqual = arg <= 5;
+  REQUIRE(lessEqual(4));
+  REQUIRE(lessEqual(5));
+  REQUIRE_FALSE(lessEqual(6));
+
+  constexpr auto compareThreeWay = arg <=> 5;
+  REQUIRE(std::is_eq(compareThreeWay(5)));
+  REQUIRE(std::is_lt(compareThreeWay(3)));
+  REQUIRE(std::is_gt(compareThreeWay(7)));
+
+  constexpr auto bitwiseAnd = arg & 5;
+  REQUIRE(bitwiseAnd(0b1010) == (0b1010 & 5));
+
+  constexpr auto bitwiseOr = arg | 5;
+  REQUIRE(bitwiseOr(0b1010) == (0b1010 | 5));
+
+  constexpr auto bitwiseXor = arg ^ 5;
+  REQUIRE(bitwiseXor(0b1010) == (0b1010 ^ 5));
+
+  constexpr auto bitwiseNot = ~arg;
+  REQUIRE(bitwiseNot(0b1010) == ~0b1010);
+
+  constexpr auto logicalAnd = arg && true;
+  REQUIRE(logicalAnd(true));
+  REQUIRE_FALSE(logicalAnd(false));
+
+  constexpr auto logicalOr = arg || false;
+  REQUIRE(logicalOr(true));
+  REQUIRE_FALSE(logicalOr(false));
+
+  constexpr auto logicalNot = !arg;
+  REQUIRE_FALSE(logicalNot(true));
+  REQUIRE(logicalNot(false));
+}
+
+TEST_CASE("xy-z") {
+  using namespace args;
+  constexpr auto add = xy::x + xy::y;
+  REQUIRE(add(5, 10) == 15);
+
+  constexpr auto subtract = xy::x - xy::y;
+  REQUIRE(subtract(10, 5) == 5);
+
+  constexpr auto multiply = xy::x * xy::y;
+  REQUIRE(multiply(5, 10) == 50);
+
+  constexpr auto divide = xy::x / xy::y;
+  REQUIRE(divide(10, 5) == 2);
+
+  constexpr auto modulo = xy::x % xy::y;
+  REQUIRE(modulo(10, 5) == 0);
+
+  constexpr auto negate = -xy::x;
+  REQUIRE(negate(5) == -5);
+
+  constexpr auto equal = xy::x == xy::y;
+  REQUIRE_FALSE(equal(5, 10));
+  REQUIRE(equal(5, 5));
+  REQUIRE_FALSE(equal(0, 5));
+
+  constexpr auto notEqual = xy::x != xy::y;
+  REQUIRE(notEqual(5, 10));
+  REQUIRE_FALSE(notEqual(5, 5));
+  REQUIRE(notEqual(0, 5));
+
+  constexpr auto greater = xy::x > xy::y;
+  REQUIRE_FALSE(greater(5, 10));
+  REQUIRE_FALSE(greater(5, 5));
+  REQUIRE(greater(10, 5));
+
+  constexpr auto greaterEqual = xy::x >= xy::y;
+  REQUIRE_FALSE(greaterEqual(5, 10));
+  REQUIRE(greaterEqual(5, 5));
+  REQUIRE(greaterEqual(10, 5));
+
+  constexpr auto less = xy::x < xy::y;
+  REQUIRE(less(5, 10));
+  REQUIRE_FALSE(less(5, 5));
+  REQUIRE_FALSE(less(10, 5));
+
+  constexpr auto lessEqual = xy::x <= xy::y;
+  REQUIRE(lessEqual(5, 10));
+  REQUIRE(lessEqual(5, 5));
+  REQUIRE_FALSE(lessEqual(10, 5));
+
+  constexpr auto compareThreeWay = xy::x <=> xy::y;
+  REQUIRE(std::is_eq(compareThreeWay(5, 5)));
+  REQUIRE(std::is_lt(compareThreeWay(3, 5)));
+  REQUIRE(std::is_gt(compareThreeWay(7, 5)));
+
+  constexpr auto bitwiseAnd = xy::x & xy::y;
+  REQUIRE(bitwiseAnd(0b1010, 0b1100) == (0b1010 & 0b1100));
+
+  constexpr auto bitwiseOr = xy::x | xy::y;
+  REQUIRE(bitwiseOr(0b1010, 0b1100) == (0b1010 | 0b1100));
+
+  constexpr auto bitwiseXor = xy::x ^ xy::y;
+  REQUIRE(bitwiseXor(0b1010, 0b1100) == (0b1010 ^ 0b1100));
+
+  constexpr auto bitwiseNot = ~xy::x;
+  REQUIRE(bitwiseNot(0b1010) == ~0b1010);
+
+  constexpr auto logicalAnd = xy::x && xy::y;
+  REQUIRE_FALSE(logicalAnd(true, false));
+
+  constexpr auto logicalOr = xy::x || xy::y;
+  REQUIRE(logicalOr(true, false));
+
+  constexpr auto logicalNot = !xy::x;
+  REQUIRE_FALSE(logicalNot(true));
+
+  constexpr auto logicalNot_x = !xy::x;
+  REQUIRE(logicalNot_x(false));
+
+  constexpr auto logicalNot_x_and_y = !xy::x && !xy::y;
+  REQUIRE(logicalNot_x_and_y(false, false));
+
+  constexpr auto logicalNot_x_or_y = !xy::x || !xy::y;
+  REQUIRE_FALSE(logicalNot_x_or_y(true, true));
+
+  constexpr auto sum_of_squares = xy::x * xy::x + xy::y * xy::y;
+  REQUIRE(sum_of_squares(3, 4) == 25);
+
+  constexpr auto sum_of_squares_triplets =
+      xy::x * xy::x + xy::y * xy::y + xyz::z * xyz::z;
+  REQUIRE(sum_of_squares_triplets(3, 4, 12) == 169);
+}
+
+}  // namespace injectx::args::tests
